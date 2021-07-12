7eb75b04b7b6d7ff4212d374d4f7a3a6204af127
poniedzf <franciszek.poniedzialek@contractors.roche.com>
2021-07-12 21:49:31 +0200

    log addition

--- src/components/user/userController.ts
+++ src/components/user/userController.ts
@@ -6,7 +6,7 @@ import {getAll} from './usersService';
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const allUsers = await getAll();
-
+    console.log('some change');
     res.status(200);
     res.json( {
       results: allUsers
